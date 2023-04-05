c29e272271bd68d9094eefdc6970dae7c211fe27
Firdosh <115990041+firdoshgithubdrive@users.noreply.github.com>
2023-04-05 20:04:47 +0530

    Add files via upload

---
+++ src/assets/data/carData.js
@@ -0,0 +1,144 @@
+// import all images from assets/images directory
+
+
+
+import img01 from "../all-images/cars-img/mercedes-offer.png";
+import img02 from "../all-images/cars-img/bently-offer.png";
+import img03 from "../all-images/cars-img/bmw-offer.png";
+import img04 from "../all-images/cars-img/nissan-offer.png";
+import img05 from "../all-images/cars-img/offer-toyota.png";
+import img06 from "../all-images/cars-img/grand-i10.png";
+import img07 from "../all-images/cars-img/toyota-offer-2.png";
+import img08 from "../all-images/cars-img/rolls-royce.png";
+
+const carData = [
+  {
+    id: 1,
+    brand: "Mercedes",
+    rating: 112,
+    carName: "Mercedes bench",
+    imgUrl: img01,
+    model: "Model 3",
+    price: 30,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 2,
+    brand: "Bently",
+    rating: 102,
+    carName: "Bently Bentayga",
+    imgUrl: img02,
+    model: "Model-2022",
+    price: 35,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 3,
+    brand: "Hyundai",
+    rating: 132,
+    carName: "Hyundai Palisade",
+    imgUrl: img03,
+    model: "Model-2022",
+    price: 15,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 4,
+    brand: "Mastang",
+    rating: 102,
+    carName: "Manstang Gt-200",
+    imgUrl: img04,
+    model: "Model-2022",
+    price: 35,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 5,
+    brand: "MiniCoper",
+    rating: 94,
+    carName: "Minicoper S-100",
+    imgUrl: img05,
+    model: "Model-2022",
+    price: 30,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 6,
+    brand: "Hyundai",
+    rating: 119,
+    carName: "Grand-i10",
+    imgUrl: img06,
+    model: "Model-2022",
+    price: 20,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 7,
+    brand: "Toyota",
+    rating: 82,
+    carName: "Fortunar",
+    imgUrl: img07,
+    model: "Model 3",
+    price: 20,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+
+  {
+    id: 8,
+    brand: "Colorado",
+    rating: 52,
+    carName: "Rolls Royce Colorado",
+    imgUrl: img08,
+    model: "Model 3",
+    price: 25,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+  },
+];
+
+export default carData;
